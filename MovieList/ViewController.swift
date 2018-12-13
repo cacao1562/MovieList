b8348df3c1b4a9d146cff9faeb600a15c5a38d00
hwanungyu <cacao1562@naver.com>
2018-12-13 22:02:08 +0900

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by hwan ung Yu on 13/12/2018.
+//  Copyright © 2018 hwan ung Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
